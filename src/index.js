d102559a98761f142e3fee6f63f01a4e5ed031bd
priyank-rege <32599901+priyank-rege@users.noreply.github.com>
2019-04-09 02:44:55 -0500

    Adding data to datamodel and the query and mutation

--- src/index.js
+++ src/index.js
@@ -1,45 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}];
-
-let idCount = links.length;
-// 2
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: ()=>links,
-        link:(parent,args)=>links.filter(link=>link.id === args.id)[0]
-    },
-    Mutation:{
-        post: (parent,args) =>{
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            };
-            links.push(link);
-            return link;
-        },
-        update:(parent,args)=>{
-            let linkToUpdate = links.filter(link=>link.id === args.id)[0];
-            if(args.description){
-                linkToUpdate.description = args.description;
-            }
-            if(args.url){
-                linkToUpdate.url = args.url;
-            }
-            return linkToUpdate;
-        }
-    }
+    Query,
+    Mutation,
+    User,
+    Link
 };
 
 // 3
 const server = new GraphQLServer({
     typeDefs:'./src/schema.graphql',
     resolvers,
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        }
+    },
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`))
